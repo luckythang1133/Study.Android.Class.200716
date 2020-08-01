dc81948b18f098be96ec5d65c3ad9a7c5ad39c2b
Luckythang1133 <luckythang1133@gmail.com>
2020-08-01 17:36:29 +0700

    ...

--- Buoi2_200718/app/src/main/java/com/quangthang/testcreateproct/MainActivity.java
+++ Buoi2_200718/app/src/main/java/com/quangthang/testcreateproct/MainActivity.java
@@ -2,13 +2,64 @@ package com.quangthang.testcreateproct;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnOpenDialog1, btnOpenDialog2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addViews();
+        addEvents();
     }
+
+    private void addViews() {
+        btnOpenDialog1 = findViewById(R.id.btnOpenDialog1);
+        btnOpenDialog2 = findViewById(R.id.btnOpenDialog2);
+
+    }
+
+    private void addEvents() {
+        btnOpenDialog1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.custom_dialog);
+                dialog.setCanceledOnTouchOutside(false);
+
+                ImageView imgOK = dialog.findViewById(R.id.imgOK);
+                imgOK.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        finish();
+                    }
+                });
+
+                ImageView imgCancel = dialog.findViewById(R.id.imgCancel);
+                imgCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+        btnOpenDialog2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyCustomDialog myCustomDialog = new MyCustomDialog(MainActivity.this);
+                myCustomDialog.show();
+            }
+        });
+    }
+
 }
--- Buoi2_200718/app/src/main/java/com/quangthang/testcreateproct/MyCustomDialog.java
+++ Buoi2_200718/app/src/main/java/com/quangthang/testcreateproct/MyCustomDialog.java
@@ -1,4 +1,43 @@
 package com.quangthang.testcreateproct;
 
-public class MyCustomDialog {
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+public class MyCustomDialog extends Dialog {
+
+    Context context;
+    ImageView imgOK, imgCancel;
+    public MyCustomDialog(@NonNull Context context) {
+        super(context);
+        this.context = context;
+        linkViews();
+        setCanceledOnTouchOutside(false);
+        addEvent();
+    }
+
+
+    private void linkViews() {
+        imgOK = findViewById(R.id.imgOK);
+        imgCancel = findViewById(R.id.imgCancel);
+    }
+
+    private void addEvent() {
+        imgOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.finish();
+            }
+        });
+        imgCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+    }
 }
